3ed2bfc7287a8813f7df9b512eea5017c43b96cf
NithinSastry <nithin_sastry@outlook.com>
2019-03-21 22:47:55 +0530

    Comment component basic, integration with faker.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./jsx-project/index"
+//import App from "./jsx-project/index"
+import App from "./components-project/index"
 
 ReactDOM.render(<App />, document.querySelector("#root"));
