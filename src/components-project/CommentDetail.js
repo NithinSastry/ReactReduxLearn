d3e4efc379fdbf98a5c7aafcd6c25897938f51a1
NithinSastry <nithin_sastry@outlook.com>
2019-03-24 20:36:54 +0530

    passing all props for comment detail component.

--- src/components-project/CommentDetail.js
+++ src/components-project/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from "react";
-import faker from "faker";
 
 const commentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatarSource} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 9:00PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Random comment</div>
+        <div className="text">{props.comment}</div>
       </div>
     </div>
   );
--- src/components-project/index.js
+++ src/components-project/index.js
@@ -5,11 +5,37 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author = {faker.name.firstName()}/>
-      <CommentDetail author = {faker.name.firstName()}/>
-      <CommentDetail author = {faker.name.firstName()}/>
-      <CommentDetail author = {faker.name.firstName()}/>
-      <CommentDetail author = {faker.name.firstName()}/>
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo={faker.date.past().toString()}
+        avatarSource={faker.image.avatar()}
+        comment={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo={faker.date.past().toString()}
+        avatarSource={faker.image.avatar()}
+        comment={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo={faker.date.past().toString()}
+        avatarSource={faker.image.avatar()}
+        comment={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo={faker.date.past().toString()}
+        avatarSource={faker.image.avatar()}
+        comment={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo={faker.date.past().toString()}
+        avatarSource={faker.image.avatar()}
+        comment={faker.lorem.sentence()}
+      />
+      
     </div>
   );
 };
