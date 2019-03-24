b23064e0960346dbc4f491bb8874cbf44bd07fdc
NithinSastry <nithin_sastry@outlook.com>
2019-03-24 21:22:38 +0530

    Added ability to render custom components as children.

--- src/components-project/index.js
+++ src/components-project/index.js
@@ -1,41 +1,54 @@
 import React from "react";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo={faker.date.past().toString()}
-        avatarSource={faker.image.avatar()}
-        comment={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo={faker.date.past().toString()}
-        avatarSource={faker.image.avatar()}
-        comment={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo={faker.date.past().toString()}
-        avatarSource={faker.image.avatar()}
-        comment={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo={faker.date.past().toString()}
-        avatarSource={faker.image.avatar()}
-        comment={faker.lorem.sentence()}
-      />
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo={faker.date.past().toString()}
-        avatarSource={faker.image.avatar()}
-        comment={faker.lorem.sentence()}
-      />
-      
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo={faker.date.past().toString()}
+          avatarSource={faker.image.avatar()}
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        Some other harcorded text as component
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo={faker.date.past().toString()}
+          avatarSource={faker.image.avatar()}
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo={faker.date.past().toString()}
+          avatarSource={faker.image.avatar()}
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo={faker.date.past().toString()}
+          avatarSource={faker.image.avatar()}
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo={faker.date.past().toString()}
+          avatarSource={faker.image.avatar()}
+          comment={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
