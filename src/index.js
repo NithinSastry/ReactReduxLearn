ac1bae3e33e5c763af94f108f3da0fa6fd1e6b51
NithinSastry <nithin_sastry@outlook.com>
2019-03-25 21:04:53 +0530

    Seasons app.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //import App from "./jsx-project/index"
-import App from "./components-project/index"
+//import App from "./components-project/index"
+import App from "./seasons/index";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
