370b6373a95ce020296be785fd835f83d46f0c1e
Nithin Sastry <nithin_sastry@outlook.com>
2019-04-13 19:57:50 +0530

    created images project.

---
+++ src/images-project/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Images = props => {
+  return <div>Images project</div>;
+};
+
+export default Images;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import App from "./jsx-project/index"
 //import App from "./components-project/index"
-import App from "./seasons/index";
+//import App from "./seasons/index";
+import App from "./images-project/index";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
