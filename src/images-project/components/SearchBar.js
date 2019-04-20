08572f30c2f380d4c4bf2a9e3ac2e2bb6d176fcd
Nithin Sastry <nithin_sastry@outlook.com>
2019-04-20 21:45:07 +0530

    styles for search box and updated package.json

--- src/images-project/components/SearchBar.js
+++ src/images-project/components/SearchBar.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    return <div>Search bar component</div>;
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="">Image search</label>
+            <input type="text" placeholder="Search here" className="" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
