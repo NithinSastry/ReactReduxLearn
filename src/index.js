d699504e2d23348f70608f9338f7635d0902f94c
Nithin Sastry <nithin_sastry@outlook.com>
2019-04-21 22:00:09 +0530

    Created search bar in youtube video search app.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 //import App from "./jsx-project/index"
 //import App from "./components-project/index"
 //import App from "./seasons/index";
-import App from "./images-project/index";
+// import App from "./images-project/index";
+import App from "./youtube-project/index";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ src/youtube-project/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onSearchChange = e => {
+    this.setState({
+      term: e.target.value
+    });
+  };
+
+  onSearchSubmit = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onSearchSubmit}>
+          <div className="field">
+            <label htmlFor="videoSearch">Video Search</label>
+            <input
+              name="videoSearch"
+              type="text"
+              value={this.state.term}
+              onChange={this.onSearchChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
