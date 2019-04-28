3fb0407c9863bc44de6465e9876b78f9ce6e898c
Nithin Sastry <nithin_sastry@outlook.com>
2019-04-29 03:04:12 +0530

    fetching users who are unique without memoize.

--- src/blog-redux-project/actions/index.js
+++ src/blog-redux-project/actions/index.js
@@ -1,12 +1,17 @@
 import _ from "lodash";
 import JSONApi from "../api";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(userId => dispatch(fetchUser(userId)));
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await JSONApi.get("/posts");
   dispatch({ type: "FETCH_POSTS_SUCCESS", payload: response.data });
 };
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await JSONApi.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER_SUCCESS", payload: response.data });
-});
+};
