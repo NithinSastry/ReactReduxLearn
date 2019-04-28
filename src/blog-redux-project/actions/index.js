41b54e158d5255032e909522af768c1608e47f33
Nithin Sastry <nithin_sastry@outlook.com>
2019-04-29 02:28:00 +0530

    memoized the action creator.

--- src/blog-redux-project/actions/index.js
+++ src/blog-redux-project/actions/index.js
@@ -1,9 +1,12 @@
+import _ from "lodash";
 import JSONApi from "../api";
 export const fetchPosts = () => async dispatch => {
   const response = await JSONApi.get("/posts");
   dispatch({ type: "FETCH_POSTS_SUCCESS", payload: response.data });
 };
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await JSONApi.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER_SUCCESS", payload: response.data });
-};
+});
