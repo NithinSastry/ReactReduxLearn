f271da9877869d1e7994403c763b0ecbd5933ff8
Nithin Sastry <nithin_sastry@outlook.com>
2019-04-29 01:18:56 +0530

    fetch user action creator.

--- src/blog-redux-project/actions/index.js
+++ src/blog-redux-project/actions/index.js
@@ -3,3 +3,7 @@ export const fetchPosts = () => async dispatch => {
   const response = await JSONApi.get("/posts");
   dispatch({ type: "FETCH_POSTS_SUCCESS", payload: response.data });
 };
+export const fetchUser = id => async dispatch => {
+  const response = await JSONApi.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
