c28aab03c18da6012a66447259c248225da61b09
Nithin Sastry <nithin_sastry@outlook.com>
2019-06-24 00:04:17 +0530

    Scafolding components.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 //import App from "./jsx-project/index"
 //import App from "./components-project/index"
 //import App from "./seasons/index";
 //import App from "./images-project/index";
 //import App from "./youtube-project/index";
 //import App from "./songs-redux-project";
-import BlogReduxProject from "./blog-redux-project";
+//import BlogReduxProject from "./blog-redux-project";
+import App from './router-project';
 
-ReactDOM.render(<BlogReduxProject />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ src/router-project/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <p>Hello from router project.</p>
+    </React.Fragment>
+  );
+};
+
+export default App;
