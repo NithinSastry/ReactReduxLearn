bec5982231a9a0daf5b59551ae446fb19951ea96
Nithin Sastry <nithin_sastry@outlook.com>
2019-06-24 14:23:58 +0530

    bug fix.

--- src/router-project/components/GoogleAuth.js
+++ src/router-project/components/GoogleAuth.js
@@ -39,13 +39,13 @@ class GoogleAuth extends React.Component {
       return null;
     } else if (this.state.isSignedIn === true) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon">Sign out</i>
         </button>
       );
     } else {
       return (
-        <button onClick={this.onSignIn} className="ui red google button">
+        <button onClick={this.onSignInClick} className="ui red google button">
           <i className="google icon">Sign in with google</i>
         </button>
       );
